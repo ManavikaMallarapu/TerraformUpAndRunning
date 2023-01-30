510475d828ddca03c8af3b26369f700844975e54
ManavikaMallarapu <mxm35570@ucmo.edu>
2023-01-30 14:54:43 -0600

    assignment1

---
+++ Chapter1/Assignment1.tf
@@ -0,0 +1,172 @@
+#aws provider in ohio region
+provider "aws" {
+  region = "us-east-2"
+}
+
+#aws create two subnets with one public and one private, create ec2 instance in public subnet and install mongodb in private subnet, install NAT gateway in private subnet and route table in public subnet to access internet from private subnet and vice versa and create security group to allow inbound traffic from public subnet to private subnet and vice versa
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  tags = {
+    Name = "vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-2a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public_subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  cidr_block = "10.0.15.0/24"
+  availability_zone = "us-east-2b"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "private_subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  tags = {
+    Name = "igw"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  route {
+    cidr_block = "10.0.0.0/16"
+    gateway_id = "${aws_internet_gateway.igw.id}"
+  }
+  tags = {
+    Name = "public_route_table"
+  }
+}
+
+resource "aws_route_table_association" "public_route_table_association" {
+  subnet_id = "${aws_subnet.public_subnet.id}"
+  route_table_id = "${aws_route_table.public_route_table.id}"
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = "${aws_eip.nat_eip.id}"
+  subnet_id = "${aws_subnet.public_subnet.id}"
+  tags = {
+    Name = "nat_gateway"
+  }
+}
+
+resource "aws_eip" "nat_eip" {
+  vpc = true
+  tags = {
+    Name = "nat_eip"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  route {
+    cidr_block = "10.0.0.0/16"
+    gateway_id = "${aws_nat_gateway.nat_gateway.id}"
+  }
+  tags = {
+    Name = "private_route_table"
+  }
+}
+
+resource "aws_route_table_association" "private_route_table_association" {
+  subnet_id = "${aws_subnet.private_subnet.id}"
+  route_table_id = "${aws_route_table.private_route_table.id}"
+}
+
+resource "aws_security_group" "public_security_group" {
+  name = "public_security_group"
+  description = "Allow inbound traffic from public subnet to private subnet"
+  vpc_id = "${aws_vpc.vpc.id}"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["10.0.0.0/24"]
+  }
+  ingress {
+    from_port = 27017
+    to_port = 27017
+    protocol = "tcp"
+    cidr_blocks = ["10.0.0.0/24"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "public_security_group"
+  }
+}
+
+resource "aws_security_group" "private_security_group" {
+  name = "private_security_group"
+  description = "Allow inbound traffic from private subnet to public subnet"
+  vpc_id = "${aws_vpc.vpc.id}"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["10.0.0.0/24"]
+  }
+  ingress {
+    from_port = 27017
+    to_port = 27017
+    protocol = "tcp"
+    cidr_blocks = ["10.0.0.0/24"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "private_security_group"
+  }
+}
+
+resource "aws_instance" "public_instance" {
+  ami = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  key_name = "terraform"
+  subnet_id = "${aws_subnet.public_subnet.id}"
+  vpc_security_group_ids = ["${aws_security_group.public_security_group.id}"]
+  tags = {
+    Name = "public_instance"
+  }
+}
+
+resource "aws_instance" "private_instance" {
+  ami = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  key_name = "terraform"
+  subnet_id = "${aws_subnet.private_subnet.id}"
+  vpc_security_group_ids = ["${aws_security_group.private_security_group.id}"]
+  tags = {
+    Name = "private_instance"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y mongodb",
+    ]
+  }
+}
+
+
